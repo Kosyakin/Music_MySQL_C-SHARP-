b3862d1fa40c0117b105f8b127f73efa2150abc2
Vladimir <62693680+Kosyakin@users.noreply.github.com>
2021-05-11 13:11:21 +0300

    Update Form2.Designer.cs

--- Music/Form2.Designer.cs
+++ Music/Form2.Designer.cs
@@ -49,7 +49,7 @@
             this.button1.TabIndex = 0;
             this.button1.Text = "Добавить трек в плейлист";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.Click += new System.EventHandler(this.Batton_From1_Load);
             // 
             // dataGridView1
             // 
