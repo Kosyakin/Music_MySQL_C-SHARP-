9d024de62d5e185fae08ae40a21a17ee267b1c50
Vladimir <62693680+Kosyakin@users.noreply.github.com>
2021-05-11 13:10:09 +0300

    Update Form2.cs

--- Music/Form2.cs
+++ Music/Form2.cs
@@ -93,7 +93,7 @@ namespace Music
 
     
         
-        private void button1_Click(object sender, EventArgs e)
+          private void Batton_From1_Load(object sender, EventArgs e)
         {
             Form1 f1 = new Form1();
             f1.Show();
